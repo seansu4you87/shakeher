8eaa5959bfe8a4692704d38fab0f88909e748922
codeb87 <codeb87@5c8ecea4-87e4-11de-b5b6-bfb8bb3c0290>
2009-08-13 09:07:56 +0000

    and so it begins

---
+++ shaker/Classes/shakerViewController.h
@@ -0,0 +1,16 @@
+//
+//  shakerViewController.h
+//  shaker
+//
+//  Created by Ben Cunningham on 8/13/09.
+//  Copyright Stanford University 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface shakerViewController : UIViewController {
+
+}
+
+@end
+
